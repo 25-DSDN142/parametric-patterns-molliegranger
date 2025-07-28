b8c386174ff428777cc11a3545c107266c4a2a2d
molliegranger <Molliemaygranger@gmail.com>
2025-07-29 10:04:36 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,7 +1,11 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 30;
+let circleSize = 60
 
+let rayL = 10
+let rayL2 = 190
+//diagionals 
+let rayL3 = 40
+let rayL4 = 160
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -17,9 +21,26 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(212, 243, 255); //sky blue
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+strokeWeight(0);
+fill (255, 200, 0)
+
+circle(100,100, circleSize);
+
+quad(90, 100, 100, rayL, 110, 100, 100, rayL2); //vertical
+quad(100, 90, rayL, 100, 100, 110, rayL2, 100); // horisontal
+
+
+ push()
+ translate(100,100)
+ rotate(45)
+ quad(100-90, 100-100, 100-100, 100-rayL3, 100-110, 100-100, 100-100, 100-rayL4); //vertical
+ quad(100-100, 100-90, 100-rayL3, 100-100, 100-100, 100-110, 100-rayL4, 100-100); // horisontal
+
+ pop()
+
+
 }
