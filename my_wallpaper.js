ce7be2920c04ad9d26acc3b11903729100b90a52
molliegranger <Molliemaygranger@gmail.com>
2025-08-04 16:36:25 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -2,16 +2,18 @@
 let circleSize = 70 //changes size of center of sun
 
 // ray lengths
-let rayL = 190 // change length of east and south pointing rays
-let rayL2 = 10 // change length of north and west pointing rays
-let rayL3 = 190 // change length of se and sw pointing rays
-let rayL4 = 10 // change length of nw and ne pointing rays
+let rayL = 100 // change length of east and south pointing rays
+let rayL2 = 100 // change length of north and west pointing rays
+let rayL3 = 0 // change length of se and sw pointing rays
+let rayL4 = 0 // change length of nw and ne pointing rays
 
 let day = false //change between day and night 
-let storm = false //change clouds to night or stormy
+let darkcloud = false //change clouds to night or stormy
 
-let showbigstar = false // turn sun/star on or off
+let showbigstar = true // turn sun/star on or off
 let showlittlestars = true // turn little star on or off
+let showcloud = false //turn cloud on or off
+let crescent = true // turns moon into crescent shape when true
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(GRID_WALLPAPER);
@@ -31,30 +33,34 @@ background(29, 58, 107); //night sky
   if(day == true){
   background(204, 235, 252); //day sky  
   }
-if (day == false && showlittlestars == true) {
- fill(252, 238, 177); // soft white-yellow
- circle(30, 40, 10);
- circle(60, 70, 2);
- circle(150, 50, 2);
- circle(100, 120, 3);
- circle(170, 180, 2);
- }
 }
-
-
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
 strokeWeight(0);
+if (day == false && showlittlestars == true) {
+ fill(252, 238, 177); // soft white-yellow
+ circle(30, 40, 10);
+ circle(60, 70, 3);
+ circle(150, 50, 3);
+ circle(100, 120, 4);
+ circle(170, 180, 3);
+
+ quad(38, 150, 40, 160, 42, 150, 40, 140); //vertical rays
+ quad(40, 148, 50, 150, 40, 152, 30, 150); // horisontal rays
+ }
 
 if (showbigstar ==true){
  if (day == true){
  fill (255, 200, 0);// sun colour
  } 
-
  if (day == false) {
  fill(252, 238, 177); // star colour
- } 
-circle(100,100, circleSize); //middle of sun/star
+ }
+ circle(100,100, circleSize); //middle of sun/star
+ if (day == false && crescent == true) {
+ fill(29, 58, 107); // background sky color
+ circle(92, 100, circleSize); // overlapping shape to cut crescent
+  }
 
 quad(90, 100, 100, rayL, 110, 100, 100, rayL2); //vertical rays
 quad(100, 90, rayL, 100, 100, 110, rayL2, 100); // horisontal rays
@@ -67,12 +73,17 @@ quad(100, 90, rayL, 100, 100, 110, rayL2, 100); // horisontal rays
 
  pop()
 }
-fill(255); // night cloud colour
- if (storm == true) {
- fill(112, 128, 156); // day cloud colour
+
+if (showcloud == true) {
+ if (darkcloud == true) {
+ fill(112, 128, 156); // stormy/night cloud colour
+ }
+ if (darkcloud == false) {
+ fill(255); // day cloud colour
  }
 
-ellipse(-20, 5, 35);
-ellipse(0, 0, 40, 48);
-ellipse(20, 5, 35);
+ ellipse(-20, 5, 35);
+ ellipse(0, 0, 40, 48);
+ ellipse(20, 5, 35);
+}
 }
