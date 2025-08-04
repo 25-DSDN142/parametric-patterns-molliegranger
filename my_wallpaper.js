4b4a72644901370bcbf8308d4513445acff632c1
molliegranger <Molliemaygranger@gmail.com>
2025-08-04 18:34:11 +1200

    final turn in

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,25 +1,26 @@
 //your parameter variables go here!
-let circleSize = 70 //changes size of center of sun
+//big sun/star/moon apperance
+let circleSize = 70 //size of sun/star center
+let showbigstar = true // show or hide big sun/star
+let crescent = true // show cresent moon shape if night 
 
-// ray lengths
-let rayL = 100 // change length of east and south pointing rays
-let rayL2 = 100 // change length of north and west pointing rays
-let rayL3 = 0 // change length of se and sw pointing rays
-let rayL4 = 0 // change length of nw and ne pointing rays
+//ray lengths
+let rayL = 100 // east and south pointing rays
+let rayL2 = 100 // north and west pointing rays
+let rayL3 = 0 // south-east and south-west pointing rays
+let rayL4 = 0 // north-west and north-east pointing rays
 
-let day = false //change between day and night 
-let darkcloud = false //change clouds to night or stormy
+//sky and weather
+let day = false //change between day and night background
+let showcloud = false //show or hide clouds
+let darkcloud = false //cloud colour, true: night/stormy, false: day/white 
 
-let showbigstar = true // turn sun/star on or off
-let showlittlestars = true // turn little star on or off
-let showcloud = false //turn cloud on or off
-let crescent = true // turns moon into crescent shape when true
+//small stars
+let showlittlestars = true // show or hide little star
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(GRID_WALLPAPER);
- 
-  
-  pWallpaper.resolution(NINE_LANDSCAPE);
+  pWallpaper.resolution(A3);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
@@ -37,18 +38,20 @@ background(29, 58, 107); //night sky
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
 strokeWeight(0);
+//little stars
 if (day == false && showlittlestars == true) {
- fill(252, 238, 177); // soft white-yellow
+ fill(252, 238, 177); // star colour
  circle(30, 40, 10);
  circle(60, 70, 3);
  circle(150, 50, 3);
  circle(100, 120, 4);
  circle(170, 180, 3);
-
+ 
+ //small sparkle rays
  quad(38, 150, 40, 160, 42, 150, 40, 140); //vertical rays
  quad(40, 148, 50, 150, 40, 152, 30, 150); // horisontal rays
  }
-
+// sun/star/moon
 if (showbigstar ==true){
  if (day == true){
  fill (255, 200, 0);// sun colour
@@ -56,12 +59,15 @@ if (showbigstar ==true){
  if (day == false) {
  fill(252, 238, 177); // star colour
  }
- circle(100,100, circleSize); //middle of sun/star
+
+ circle(100,100, circleSize); //center of sun/star
+
  if (day == false && crescent == true) {
- fill(29, 58, 107); // background sky color
- circle(92, 100, circleSize); // overlapping shape to cut crescent
+ fill(29, 58, 107); // night sky color to cut out cresent
+ circle(92, 100, circleSize); 
   }
 
+//star/sun rays 
 quad(90, 100, 100, rayL, 110, 100, 100, rayL2); //vertical rays
 quad(100, 90, rayL, 100, 100, 110, rayL2, 100); // horisontal rays
 
@@ -73,7 +79,7 @@ quad(100, 90, rayL, 100, 100, 110, rayL2, 100); // horisontal rays
 
  pop()
 }
-
+// clouds
 if (showcloud == true) {
  if (darkcloud == true) {
  fill(112, 128, 156); // stormy/night cloud colour
